742417bb55bc9e80bc93f3f8b5c621ccb6aa00dc
Suriyaravichandran <rsuriya119@gmail.com>
2023-08-16 20:10:14 +0530

    update

--- trafficsignal.py
+++ trafficsignal.py
@@ -1,20 +1,26 @@
-#trafic signal program
+# Traffic signal program with loop
+print("Press 1 for red, 2 for yellow, 3 for green, 4 to turn on the signal, 5 to turn off the signal, or 0 to exit")
+while True:
+    # Display rules
+    
 
-#it is rules
-print("press 1 is red,2 is yellow,3 is green, 4 is on the signal,5 is off the signal")
+    # Get input from user
+    btn = int(input("Enter your option: "))
 
-#get input to user
-btn=int(input("Enter your option:"))
-
-# work flow of this project
-if(btn==1):
-    print("Red")
-elif(btn==2):
-    print("Yellow")
-elif(btn==3):
-    print("Green")
-elif(btn==4):
-    print("signal is on")
-else:
-    print("signal is off please turn on signal")
+    # Workflow of this project
+    if btn == 1:
+        print("Red")
+    elif btn == 2:
+        print("Yellow")
+    elif btn == 3:
+        print("Green")
+    elif btn == 4:
+        print("Signal is on")
+    elif btn == 5:
+        print("Signal is off. Please turn on the signal.")
+    elif btn == 0:
+        print("Exiting the program.")
+        break  # Exit the loop if the user chooses to exit
+    else:
+        print("Invalid option")
 
